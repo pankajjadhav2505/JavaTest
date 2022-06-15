05b83858c2be77dfa9d1ebe8e7e59f4577a69827
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2022-06-15 10:32:17 +0530

    Add files via upload

---
+++ ResourceLeak.java
@@ -0,0 +1,147 @@
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Closer;
+
+import java.io.*;
+
+class ResourceLeak {
+    public void process1() {
+
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("out.txt", true)));
+            out.println("the text");
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK
+            out.close();  //close() is in try clause
+
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+            FileOutputStream in = new FileOutputStream("xanadu.txt");
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK
+            in.close();
+        } catch (IOException e) {
+        }
+
+    }
+
+    public void processk() {
+
+        // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+        PrintWriter out = null;
+        try {
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+            out = new PrintWriter(new File(""));
+            out.println("the text");
+        } catch (IOException e) {
+        } finally {
+            if (true) {
+                // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+                out.close();
+            }
+        }
+    }
+
+    public void testExceptionBlock() throws IOException {
+
+        Reader reader = null;
+        try {
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+            reader = new FileReader("");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            //Need to fix this
+            reader.close();
+        } finally {
+            if (reader != null) {
+                try {
+                    int k;
+                    int t;
+                    int kk;
+                    int tt;
+                    // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+                    reader.close();
+                } catch (IOException e) {
+                } finally {
+
+                }
+            }
+        }
+    }
+
+    //Non-compliant code
+    //resource is not closed anywhere
+    public void process2() {
+
+        try {
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("out.txt", true)));
+            out.println("the text");
+        } catch (IOException e) {
+        }
+
+    }
+
+    public void process3() {
+
+        // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+        try (PrintWriter out2 = new PrintWriter(new File(""))) {
+//			out2.println("the text");
+        } catch (IOException e) {
+        }
+
+    }
+
+    PrintWriter outk = null;
+
+    public void processK() {
+
+        try {
+            // EMB-ISSUE: CodeIssueNames.RESOURCE_LEAK/no-detect
+            if (null != outk)
+                outk.close();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void processN() throws IOException {
+        Closer closer = Closer.create();
+        try {
+            InputStream in = closer.register(openInputStream());
+            OutputStream out = closer.register(openOutputStream());
+            // do stuff
+        } catch (
+                Throwable e) {
+            // ensure that any checked exception types other than IOException that could be thrown are
+            // provided here, e.g. throw closer.rethrow(e, CheckedException.class);
+            throw closer.rethrow(e);
+        } finally {
+            closer.close();
+        }
+    }
+
+    private OutputStream openOutputStream() {
+        return null;
+    }
+
+    public long processN1() throws IOException {
+	Closer closer = Closer.create();
+    try {
+        InputStream input = (InputStream) closer.register(openInputStream());
+        OutputStream out = closer.register(openStream());
+		long written = ByteStreams.copy(input, out);
+		out.flush(); // https://code.google.com/p/guava-libraries/issues/detail?id=1330
+		return written;
+	} catch (Throwable e) {
+		throw closer.rethrow(e);
+	} finally {
+		closer.close();
+	}
+}
+
+    private OutputStream openStream() {
+        return null;
+    }
+
+    private InputStream openInputStream() {
+        return null;
+    }
+}
