673fb839b429af72e84b4b3382336709cb63fbc2
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2022-06-24 23:00:47 +0530

    Update ByteSource.java

--- ByteSource.java
+++ ByteSource.java
@@ -76,12 +76,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 @GwtIncompatible
 @ElementTypesAreNonnullByDefault
 public abstract class ByteSource {  
-  
-  
-  
-  
-  
-  /** Constructor for use by subclasses. */
+ /** Constructor for use by subclasses. */
   protected ByteSource() {}
   /**
    * Returns a {@link CharSource} view of this byte source that decodes bytes read from this source
