9817efa63aa624a36f8cb61307d7a0bb313e96a2
Pooja Jadhav <jadhavpoojasopan@gmail.com>
2022-10-12 12:40:49 +0530

    sout

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -56,6 +56,10 @@ public class Category extends Vehicle implements NewInterface {
             this.x = x - 1;
 
         System.out.println("X and Y are different");
+        System.out.println("X and Y are different");
+
+        System.out.println("X and Y are different");
+
         return false;
     }
 
